572fb555f845cee9be1f50a1d06bb39b0d8a2cf5
ErsaGunTosun <ersagunfatihdev@gmail.com>
2024-01-02 20:37:49 +0300

    variables

---
+++ 01_variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "ErsaGun"
+	// var(variable) variable_name variable_type = value
+
+	var surname string
+	surname = "Tosun"
+
+	age := 20
+	// var(variable) variable_name := value
+
+	fmt.Println("Run", name, surname, age)
+}
