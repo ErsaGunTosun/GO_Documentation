b89e4df9a430efa2fab57d873971e995f6cc420d
ErsaGun Tosun <ersagunfatihdev@gmail.com>
2024-02-21 23:51:30 -0800

    Packages

---
+++ 22_packages/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Pakit kullanım sebeblerimiz
+	/*
+		- Kodun tekrar kullanılabilirliğini arttırır.
+		- Kodun okunabilirliğini arttırır.
+		- Kod daha modüler hale gelir.
+		- Kodun bakımı daha kolay olur.
+	*/
+
+	fmt.Println("Main Package")
+}
