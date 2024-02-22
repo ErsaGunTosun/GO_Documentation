a20c9602ae1918f45c2cb9dc8b5577e027517b17
ErsaGun Tosun <ersagunfatihdev@gmail.com>
2024-02-22 00:10:52 -0800

    My Package

---
+++ 23_myPackage/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"video/meraba"
+)
+
+func main() {
+	meraba.Meraba()
+	fmt.Println("Main Package")
+}
---
+++ 23_myPackage/meraba/meraba.go
@@ -0,0 +1,15 @@
+package meraba
+
+import "fmt"
+
+func Meraba() {
+	fmt.Println("Meraba")
+}
+
+func Hi() {
+	fmt.Println("Hi")
+}
+
+func Hello() {
+	fmt.Println("Hello")
+}
